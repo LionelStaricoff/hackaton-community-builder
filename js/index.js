565e1e621f8719150ef3a54dcb1f3c508cd87697
lionel staricoff <staricofflionel@hotmail.com>
2024-10-25 11:03:00 -0500

    agregando arreglo de las hojas

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@
         let range = 'Hoja1!A1:D10'; 
         range = 'Hoja1!A1:D10';
         let url = "https://docs.google.com/spreadsheets/d/18Rf0-3sREFosw__tQYaUmtzJNL3M-PLsm-HIqENf2Yw/edit?pli=1&gid=0#gid=0"
-        //const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+       const hojas = ["Comunidades","Otras (Medios, etc.)","Dropdowns","Sobre esta iniciativa"];
         
-url = 'https://opensheet.elk.sh/18Rf0-3sREFosw__tQYaUmtzJNL3M-PLsm-HIqENf2Yw/Comunidades'+'!A1:A50'
+url = `https://opensheet.elk.sh/18Rf0-3sREFosw__tQYaUmtzJNL3M-PLsm-HIqENf2Yw/${hojas[3]}`//+'!A1:A50'
         fetch(url)
           .then(response => response.json())
           .then(data => {
